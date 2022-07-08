a1ec71bc9f814dd060451202fd2adfb5dca2a337
NelsonDong41 <68518070+NelsonDong41@users.noreply.github.com>
2022-07-07 20:55:26 -0400

    revised rock paper scissor

--- Projects/Rock Paper Scissors/script.js
+++ Projects/Rock Paper Scissors/script.js
@@ -1,4 +1,41 @@
 console.log("Hello World");
+const container = document.querySelector("body");
+const rock = document.createElement("button");
+rock.setAttribute("id", "rock");
+rock.textContent = "Rock";
+rock.style.borderRadius = "5px";
+const paper = document.createElement("button");
+paper.setAttribute("id", "paper");
+paper.textContent = "Paper";
+
+const scissor = document.createElement("button");
+scissor.setAttribute("id", "scissor");
+scissor.textContent = "Scissor";
+
+container.appendChild(rock);
+container.appendChild(paper);
+container.appendChild(scissor);
+
+let count = 0;
+const countdiv = document.createElement("div");
+countdiv.textContent = count;
+
+const playerdiv = document.createElement("div");
+const computerdiv = document.createElement("div");
+const responsediv = document.createElement("div");
+
+container.appendChild(playerdiv);
+container.appendChild(computerdiv);
+container.appendChild(responsediv);
+container.appendChild(countdiv);
+
+const options = document.querySelectorAll("button");
+options.forEach((option) => {
+  option.style.cssText = "background :green; border-radius : 5px";
+  option.addEventListener("click", function (e) {
+    playRound(e.target.id, computerPlay());
+  });
+});
 
 function computerPlay() {
   let value = Math.floor(Math.random() * 3);
@@ -10,7 +47,6 @@ function computerPlay() {
   } else if (value === 2) {
     choice = "scissor";
   }
-  console.log("Computer chose : " + choice);
   return choice;
 }
 
@@ -21,8 +57,9 @@ function computerPlay() {
  */
 
 function playRound(playerSelection, computerPlay) {
+  let playerChoice = `You chose : ${playerSelection}`;
+  let computerChoice = `Computer chose : ${computerPlay}`;
   let response = "";
-  response += "You chose : " + playerSelection.toLowerCase() + "\n";
   if (
     (playerSelection.toLowerCase().match("rock") &&
       computerPlay.match("scissor")) ||
@@ -32,28 +69,14 @@ function playRound(playerSelection, computerPlay) {
       computerPlay.match("paper"))
   ) {
     response += "You Win";
+    count++;
   } else if (playerSelection.toLowerCase().match(computerPlay)) {
     response += "Tie";
   } else {
     response += "You lose";
   }
-  return response;
+  playerdiv.textContent = playerChoice;
+  computerdiv.textContent = computerChoice;
+  responsediv.textContent = response;
+  countdiv.textContent = count;
 }
-
-function game() {
-  let count = 0;
-
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Choose one of { rock, paper, scissor }");
-    const computerSelection = computerPlay();
-    const roundResult = playRound(playerSelection, computerSelection);
-    if (roundResult.match("You Win")) {
-      count++;
-    }
-    console.log(roundResult);
-  }
-
-  console.log("Number of Wins: " + count);
-}
-
-game();
