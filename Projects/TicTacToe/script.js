a7c74fe512cd22e13036737a49dbc0150e41414c
NelsonDong41 <68518070+NelsonDong41@users.noreply.github.com>
2022-07-15 18:01:34 -0400

    push

--- Projects/TicTacToe/script.js
+++ Projects/TicTacToe/script.js
@@ -1,7 +1,37 @@
+const body = document.querySelector("body");
+
 const GameBoard = (() => {
-    const array = [];
+  const array = [
+    [0, 0, 0],
+    [1, null, 1],
+    [0, 0, 0],
+  ];
+  return { array };
 })();
 
 const playerFactory = (name) => {
-    return {name};
+  return { name };
+};
+
+const Game = (() => {})();
+
+function render() {
+  let boardImage = document.createElement("section");
+  boardImage.className = "container";
+  for (let i = 0; i < GameBoard.array.length; i++) {
+    for (let j = 0; j < GameBoard.array[i].length; j++) {
+      const cell = document.createElement("div");
+      cell.className = "cell";
+      cell.setAttribute("id", `${i}, ${j}`);
+
+      if (!(GameBoard.array[i][j] == null)) {
+        cell.textContent += `${GameBoard.array[i][j] ? "O" : "X"}`;
+      }
+      cell.textContent += `${j}, ${i}`;
+      boardImage.appendChild(cell);
+    }
+  }
+  body.appendChild(boardImage);
 }
+
+render();
